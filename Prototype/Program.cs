4a994df0b193e5c35caef74f04330c7d002da94c
sergennkeles <65495192+sergennkeles@users.noreply.github.com>
2021-04-17 20:03:40 +0300

    Prototype Design Pattern

---
+++ Prototype/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer {Id=1,FirstName="Sergen",LastName="Keleş",City="İstanbul" };
+
+            Customer customer2 = (Customer)customer1.Clone();
+            customer2.FirstName = "Simge";
+            Console.WriteLine("customer1'deki veri "+customer1.FirstName+ " " +customer1.LastName);
+            Console.WriteLine("customer2'deki veri " + customer2.FirstName + " " + customer2.LastName);
+
+        }
+    }
+
+    public abstract class Person
+    {
+        public abstract Person Clone();
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class Customer: Person
+    {
+        public string City { get; set; }
+
+        public override Person Clone()
+        {
+            return (Person)MemberwiseClone();
+        }
+    }
+
+    public class Employee : Person
+    {
+        public decimal  Salary { get; set; }
+
+        public override Person Clone()
+        {
+            return (Person)MemberwiseClone();
+        }
+    }
+}
