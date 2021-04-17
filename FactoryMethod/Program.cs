645d99e5bd51c2553eb3df3a98a6777291e30150
sergennkeles <65495192+sergennkeles@users.noreply.github.com>
2021-04-17 14:11:38 +0300

    FactoryMethod Pattern

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            
+        }
+    }
+
+    public class LoggerFactory : ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            return new DatabaseLogger();
+        }
+    }
+
+
+    public class LoggerFactory2 : ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            return new FileLogger();
+        }
+    }
+    public interface ILogger
+    {
+        void Log();
+    }
+    public interface ILoggerFactory
+    {
+        ILogger CreateLogger();
+    }
+
+    public class DatabaseLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with DatabaseLogger");
+        }
+    }
+
+    public class FileLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with FileLogger");
+        }
+    }
+
+    public class CustomerManager
+    {
+        public void Add()
+        {   //LoggerFactory veya LoggerFactory2 olarak loglama sistemini 2 farklı şekilde kullanabiliriz.
+            //Dependency Injection kullanarak da yapabilirdik.
+            Console.WriteLine("Added!!!");
+            ILogger logger = new LoggerFactory2().CreateLogger(); 
+            logger.Log();
+        }
+    }
+}
