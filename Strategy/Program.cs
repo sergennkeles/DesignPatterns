315326856d5c9fd6cc6ba932a77c898aabc0a4ab
sergennkeles <65495192+sergennkeles@users.noreply.github.com>
2021-05-18 12:57:58 +0300

    Strategy Design Pattern

---
+++ Strategy/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.CreditCalculatorBase = new IndividualCustomer();
+            customerManager.Save();
+        }
+    }
+
+    abstract class CreditCalculatorBase
+    {
+        public abstract void Calculate();
+    }
+
+    class CoorporateCustomer : CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("CoorporateCustomer credit calculated!");
+        }
+    }
+
+    class IndividualCustomer : CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("IndividualCustomer credit calculated!");
+
+        }
+    }
+
+    interface ICustomerService
+    {
+        void Save();
+    }
+    class CustomerManager : ICustomerService
+    {
+        public CreditCalculatorBase CreditCalculatorBase { get; set; }
+        public void Save()
+        {
+           
+            Console.WriteLine("Customer Credit Business rules");
+            CreditCalculatorBase.Calculate();
+        }
+    }
+}
